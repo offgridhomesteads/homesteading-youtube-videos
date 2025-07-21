1e4f0147523b16e22961fff750991fae95d4d5d3
offgridhomesteads <jfisher@academyosr.com>
2025-07-20 20:30:22 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -56,6 +56,11 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
+  // Export app for Vercel serverless function
+  if (process.env.VERCEL) {
+    return app;
+  }
+
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
@@ -69,3 +74,6 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
+
+// Export for ES modules (Vercel)
+export default app;
