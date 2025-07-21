18ebcf94bf11c465900d34d84ba9b8886bff9477
offgridhomesteads <jfisher@academyosr.com>
2025-07-21 12:09:48 -0600

    Delete api/hello.js

--- api/hello.js
+++
@@ -1,4 +0,0 @@
-module.exports = function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ message: 'Hello from Vercel API', timestamp: new Date().toISOString() });
-};
