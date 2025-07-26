6e3552cd6a5528defb3a410aa0ab1a9c3110315e
offgridhomesteads <jfisher@academyosr.com>
2025-07-26 17:35:12 -0600

    Update TopicCard.tsx

--- client/src/components/TopicCard.tsx
+++ client/src/components/TopicCard.tsx
@@ -96,6 +96,16 @@ export default function TopicCard({ topic }: TopicCardProps) {
       );
     }
     
+    if (topic.slug === 'diy-home-maintenance') {
+      return (
+        <img 
+          src="/images/home-maintenance-videos.jpg"
+          alt={`${topic.name} videos`}
+          className="video-thumbnail rounded-lg w-full md:w-80 h-48 md:h-45 cursor-pointer transition-opacity hover:opacity-80 object-cover"
+        />
+      );
+    }
+    
     const svgContent = getSvgContent(topic.slug);
     if (svgContent) {
       // Scale SVG to fill the entire container (320x180)
