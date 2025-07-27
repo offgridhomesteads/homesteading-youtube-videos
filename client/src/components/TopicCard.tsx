add4a378dc0e4dbf68ff893556544b677954e6b3
offgridhomesteads <jfisher@academyosr.com>
2025-07-27 10:11:39 -0600

    Update TopicCard.tsx

--- client/src/components/TopicCard.tsx
+++ client/src/components/TopicCard.tsx
@@ -126,6 +126,16 @@ export default function TopicCard({ topic }: TopicCardProps) {
       );
     }
     
+    if (topic.slug === 'homestead-security') {
+      return (
+        <img 
+          src="/images/homestead-security-videos.jpg"
+          alt={`${topic.name} videos`}
+          className="video-thumbnail rounded-lg w-full md:w-80 h-48 md:h-45 cursor-pointer transition-opacity hover:opacity-80 object-cover"
+        />
+      );
+    }
+    
     const svgContent = getSvgContent(topic.slug);
     if (svgContent) {
       // Scale SVG to fill the entire container (320x180)
